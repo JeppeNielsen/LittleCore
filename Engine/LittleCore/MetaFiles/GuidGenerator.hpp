d5e2bcf8db1484cface151ae757b02d6cb215575
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2024-03-08 23:13:22 +0100

    added GuidGenerator header

---
+++ Engine/LittleCore/MetaFiles/GuidGenerator.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Jeppe Nielsen on 08/03/2024.
+//
+
+#pragma once
+#include <string>
+
+namespace LittleCore {
+    class GuidGenerator {
+    public:
+        std::string GenerateNew();
+    };
+}
