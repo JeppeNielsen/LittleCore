de67371c060d8f9f17d8ef316f722e42716bad36
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2024-07-01 22:34:28 +0200

    Now initializing bgfx in textureresource loader tests

--- Projects/UnitTests/Source/Tests/ResourceLoaders/TextureResourceLoaderTests.cpp
+++ Projects/UnitTests/Source/Tests/ResourceLoaders/TextureResourceLoaderTests.cpp
@@ -10,6 +10,17 @@ using namespace LittleCore;
 namespace {
     TEST(TextureResourceLoader, TestLoad) {
 
+
+        bgfx::Init init;
+        init.type = bgfx::RendererType::Noop;
+        init.resolution.reset = BGFX_RESET_VSYNC;
+        init.platformData.ndt = nullptr;
+        init.platformData.nwh = nullptr;
+        init.platformData.context = nullptr;
+        init.platformData.backBuffer = nullptr;
+        init.platformData.backBufferDS = nullptr;
+
+        bgfx::init(init);
         std::string rootPath = "../../../../Assets/";
 
         ResourcePathMapper pathMapper;
@@ -20,11 +31,7 @@ namespace {
         ResourceHandle<TextureResource> textureHandle = resourceManager.Create<TextureResource>("B62D424BF40F46359248CDE498930422");
 
         auto ig =  textureHandle->handle.idx;
-
-
-
         EXPECT_NE(textureHandle->handle.idx, bgfx::kInvalidHandle);
-
     }
 }
 
