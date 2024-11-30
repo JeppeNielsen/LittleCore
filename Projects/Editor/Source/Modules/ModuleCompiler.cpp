e91215ddb9e27f522780d3c9fee9788bc0451627
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2024-11-30 21:51:10 +0100

    now specifing -O0 no compiler optimizations

--- Projects/Editor/Source/Modules/ModuleCompiler.cpp
+++ Projects/Editor/Source/Modules/ModuleCompiler.cpp
@@ -28,7 +28,7 @@ ModuleCompilerResult ModuleCompiler::Compile(const ModuleCompilerContext &contex
     }
 
     clang += "-isysroot `xcrun -sdk macosx --show-sdk-path` ";
-    clang += "-std=c++17 -dynamiclib -Wno-return-type-c-linkage ";
+    clang += "-std=c++17 -dynamiclib -Wno-return-type-c-linkage -O0 ";
     clang += "-o " + context.outputPath + " ";
 
     std::cout << clang << std::endl;
