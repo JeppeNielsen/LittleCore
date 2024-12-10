5a17afe59ab5d1fb00613a1e15e28dc8974d74f9
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2024-12-10 08:04:47 +0100

    variadic Simulation class now defines Input, Update and Render systems

--- Engine/LittleCore/Logic/Simulation/Simulation.hpp
+++ Engine/LittleCore/Logic/Simulation/Simulation.hpp
@@ -11,7 +11,6 @@
 
 namespace LittleCore {
 
-
     template<typename ...T>
     struct SimulationSystemList {
 
@@ -26,36 +25,59 @@ namespace LittleCore {
     };
 
     template <typename ...System>
+    using InputSystems = SimulationSystemList<System...>;
+
+    template <typename ...System>
     using UpdateSystems = SimulationSystemList<System...>;
 
     template <typename ...System>
     using RenderSystems = SimulationSystemList<System...>;
 
-    template<typename UpdateSystems, typename RenderSystems>
+    template<typename InputSystems, typename UpdateSystems, typename RenderSystems>
     class Simulation {
     public:
 
         Simulation(entt::registry& registry)
                 : registry(registry),
-                  updateSystems(registry) {}
+                  inputSystems(registry),
+                  updateSystems(registry),
+                  renderSystems(registry) {}
 
+        void HandleEvent(void* event) {
+            TupleHelper::for_each(inputSystems.systems, [=] (auto& inputSystem) {
+                inputSystem.HandleEvent(event);
+            });
+        }
 
         void Update() {
-           TupleHelper::for_each(updateSystems.systems, [] (auto& updateSystem) {
+            TupleHelper::for_each(inputSystems.systems, [] (auto& inputSystem) {
+                inputSystem.Update();
+            });
+            TupleHelper::for_each(updateSystems.systems, [] (auto& updateSystem) {
                updateSystem.Update();
-           });
+            });
+            TupleHelper::for_each(renderSystems.systems, [] (auto& renderSystem) {
+                renderSystem.Update();
+            });
         }
 
         void Render(Renderer& renderer) {
-            /*TupleHelper::for_each(renderSystems.systems, [&renderer] (auto& updateSystem) {
-                updateSystem.Render(renderer);
+            TupleHelper::for_each(renderSystems.systems, [&renderer] (auto& renderSystem) {
+                renderSystem.Render(&renderer);
             });
-             */
+        }
+
+        void Render(bgfx::ViewId viewId, const WorldTransform& cameraTransform, const Camera& camera, Renderer* renderer) {
+            TupleHelper::for_each(renderSystems.systems, [&] (auto& renderSystem) {
+                renderSystem.Render(viewId, cameraTransform, camera, renderer);
+            });
         }
 
     private:
         entt::registry& registry;
+        InputSystems inputSystems;
         UpdateSystems updateSystems;
+        RenderSystems renderSystems;
     };
 
 }
