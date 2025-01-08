7e7e02c53cb000344694ba6d46f64554ad3109a3
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-01-08 08:52:13 +0100

    fixed local transform matrix not calculated correctly

--- Engine/LittleCore/Logic/Transform/LocalTransform.hpp
+++ Engine/LittleCore/Logic/Transform/LocalTransform.hpp
@@ -13,7 +13,27 @@ namespace LittleCore {
         glm::vec3 scale = vec3(1.0f,1.0f,1.0f);
 
         glm::mat4 GetLocalToParent() const {
-            return glm::scale(glm::translate(mat4x4(1.0f), position), scale) * glm::toMat4(rotation);
+            glm::quat quaternion = glm::quat(rotation);
+            glm::mat3 rotationMatrix = glm::mat3_cast(quaternion);
+            glm::mat4 result(1.0f);
+
+            result[0][0] = rotationMatrix[0][0] * scale.x;
+            result[0][1] = rotationMatrix[0][1] * scale.x;
+            result[0][2] = rotationMatrix[0][2] * scale.x;
+
+            result[1][0] = rotationMatrix[1][0] * scale.y;
+            result[1][1] = rotationMatrix[1][1] * scale.y;
+            result[1][2] = rotationMatrix[1][2] * scale.y;
+
+            result[2][0] = rotationMatrix[2][0] * scale.z;
+            result[2][1] = rotationMatrix[2][1] * scale.z;
+            result[2][2] = rotationMatrix[2][2] * scale.z;
+
+            result[3][0] = position.x;
+            result[3][1] = position.y;
+            result[3][2] = position.z;
+
+            return result;
         }
     };
 
