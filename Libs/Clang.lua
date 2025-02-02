a310392935f44a5eac0e7c280f566b7b11b40ee1
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-02-02 20:51:47 +0100

    fixed relative path

--- Libs/Clang.lua
+++ Libs/Clang.lua
@@ -1,9 +1,10 @@
 local CLANG_PATH = "../../Scripting/clang18/lib"
+local CLANG_PATH_ABSOLUTE = path.getabsolute(CLANG_PATH)
 
 function SetClangPath()
    filter "system:macosx"
       buildoptions { "-arch arm64" }
-      linkoptions { "-arch arm64 -L"..CLANG_PATH.." -rpath "..CLANG_PATH.."" }
+      linkoptions { "-arch arm64 -L"..CLANG_PATH_ABSOLUTE.." -rpath "..CLANG_PATH_ABSOLUTE.."" }
 end
 
 
