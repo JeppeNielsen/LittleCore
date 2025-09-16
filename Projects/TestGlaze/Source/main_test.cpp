153831928156278c61653f4f107852735948f277
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-09-16 22:19:23 +0200

    test now using reflection utility

--- Projects/TestGlaze/Source/main_test.cpp
+++ Projects/TestGlaze/Source/main_test.cpp
@@ -1,10 +1,8 @@
-#include <glaze/glaze.hpp>
-#include <glaze/concepts/container_concepts.hpp>
-#include <iostream>
-#include <utility>
-#include "TupleHelper.hpp"
-#include <concepts>
+#include "ReflectionUtility.hpp"
+#include <vector>
 #include "TypeUtility.hpp"
+
+using namespace LittleCore;
 
 struct Child {
     std::string name;
@@ -21,61 +19,16 @@ struct User {
     Child root;
 };
 
-template <typename T>
-concept StringLike =
-    std::same_as<std::decay_t<T>, std::string> ||
-    std::same_as<std::decay_t<T>, std::string_view> ||
-    std::is_same_v<std::remove_cvref_t<T>, char*> ||
-    std::is_same_v<std::remove_cvref_t<T>, const char*>;
-
-template <typename T>
-concept SimpleType = std::is_arithmetic_v<T> || std::is_enum_v<T>;
-
-template<typename T, typename Callable>
-void IterateMembers(T&& object, Callable&& callable) {
-
-    decltype(glz::reflect<T>()) fieldNames;
-
-    int memberIndex = 0;
-    glz::for_each_field(object, [&, callable](auto& value) {
-        using valueType = std::remove_cvref_t<decltype(value)>;
-
-        if constexpr (StringLike<valueType>) {
-            callable(fieldNames.keys[memberIndex], value);
-
-        } else if constexpr (SimpleType<valueType>) {
-            callable(fieldNames.keys[memberIndex], value);
-        } else if constexpr (glz::detail::vector_like<valueType>) {
-
-            for(auto& v : value) {
-                using containedType = std::remove_cvref_t<decltype(v)>;
-
-                if constexpr (SimpleType<containedType>) {
-                    callable(fieldNames.keys[memberIndex], v);
-                } else {
-                    IterateMembers<decltype(v)>(v, callable);
-                }
-            }
-
-        } else {
-            callable(fieldNames.keys[memberIndex], value);
-            IterateMembers(value, callable);
-        }
-        memberIndex++;
-    });
-
-}
-
 int main() {
     User u{1, "Alice", 42.0};
     u.indices = {1,2,3,4};
     u.children = {{"Jeppe", 43}, {"Ella", 9}, {"Aksel", 5}};
 
-    IterateMembers(u, [](const auto& fieldName, auto& value) {
+    ReflectionUtility::IterateMembers(u, [](const auto& fieldName, auto& value) {
         //std::cout << fieldName << " -> "<< value << " " << LittleCore::TypeUtility::GetClassName<decltype(value)>() <<  "\n";
         using type = std::remove_cvref_t<decltype(value)>;
 
-        std::cout << fieldName << " -> "<< LittleCore::TypeUtility::GetClassName<type>() <<  "\n";
+        std::cout << fieldName << " -> "<< TypeUtility::GetClassName<type>() <<  "\n";
     });
 
 }
