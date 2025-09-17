a8a7ffa8b0606de2e83c35e839544600449d1a6c
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-09-18 00:41:49 +0200

    added resizable framebuffer

---
+++ Engine/LittleCore/Logic/Rendering/ResizableFrameBuffer.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Jeppe Nielsen on 17/09/2025.
+//
+
+#include "ResizableFrameBuffer.hpp"
+
+using namespace LittleCore;
+
+void ResizableFrameBuffer::Render(uint16_t width, uint16_t height, const std::function<void()>& renderFunction) {
+    EnsureResources(width, height);
+    bgfx::setViewFrameBuffer(0, frameBuffer);
+    renderFunction();
+    bgfx::setViewFrameBuffer(0, BGFX_INVALID_HANDLE);
+}
+
+void ResizableFrameBuffer::EnsureResources(uint16_t width, uint16_t height) {
+    if (this->width == width && this->height == height) {
+        return;
+    }
+
+    this->width = width;
+    this->height = height;
+
+    if (bgfx::isValid(texture)) {
+        bgfx::destroy(texture);
+    }
+
+    if (bgfx::isValid(frameBuffer)) {
+        bgfx::destroy(frameBuffer);
+    }
+
+    const bgfx::TextureFormat::Enum kFormat = bgfx::TextureFormat::RGBA8;
+    const uint64_t kTexFlags = BGFX_TEXTURE_RT; // RT texture; sampler filtering is set when binding.
+
+    texture = bgfx::createTexture2D(
+            this->width,
+            this->height,
+            false,
+            1,
+            kFormat,
+            kTexFlags
+    );
+
+    frameBuffer = bgfx::createFrameBuffer(1, &texture, false);
+}
---
+++ Engine/LittleCore/Logic/Rendering/ResizableFrameBuffer.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Jeppe Nielsen on 17/09/2025.
+//
+
+
+#pragma once
+#include <bgfx/bgfx.h>
+#include <functional>
+
+namespace LittleCore {
+    class ResizableFrameBuffer {
+    public:
+        void Render(uint16_t width, uint16_t height, const std::function<void()>& renderFunction);
+        void EnsureResources(uint16_t width, uint16_t height);
+        uint16_t width = 0;
+        uint16_t height = 0;
+        bgfx::TextureHandle texture = BGFX_INVALID_HANDLE;
+        bgfx::FrameBufferHandle frameBuffer = BGFX_INVALID_HANDLE;
+    };
+}
