9d422ebb9346a9108feb032d30be64b4d7aa550b
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-10-14 20:33:34 +0200

    removed logs in ImguiInputHandler

--- Projects/TestNetimguiClient/Source/Gui/ImguiInputHandler.cpp
+++ Projects/TestNetimguiClient/Source/Gui/ImguiInputHandler.cpp
@@ -31,23 +31,11 @@ void ImguiInputHandler::HandleInput(void* event, Input& input) {
     for (int i = 0; i < ImGuiKey_KeysData_SIZE; ++i) {
         if (ImGui::IsKeyPressed((ImGuiKey)i, false)) {
             input.keysDown.push_back((InputKey)i);
-            std::cout << "Key down " << i << "\n";
         }
 
         if (ImGui::IsKeyReleased((ImGuiKey)i)) {
             input.keysUp.push_back((InputKey)i);
-            std::cout << "Key up " << i << "\n";
         }
-
-        /*
-        if (io.KeysDown[i] && std::find(prev.keysDown.begin(), prev.keysDown.end(),(InputKey)i) == prev.keysDown.end()) {
-            input.keysDown.push_back((InputKey)i);
-            std::cout << "Key down " << i << "\n";
-        } else if (!io.KeysDown[i] && std::find(prev.keysUp.begin(), prev.keysUp.end(),(InputKey)i) == prev.keysDown.end()) {
-            input.keysUp.push_back((InputKey)i);
-            std::cout << "Key up " << i << "\n";
-        }
-         */
     }
 
     for (int i = 0; i < 3; ++i) {
@@ -55,13 +43,10 @@ void ImguiInputHandler::HandleInput(void* event, Input& input) {
 
         if (ImGui::IsMouseClicked(i) && isMouseInWindow) {
             input.touchesDown.push_back(inputTouch);
-            std::cout << "Mouse down " << i << "\n";
         }
 
         if (ImGui::IsMouseReleased(i)) {
             input.touchesUp.push_back(inputTouch);
-            std::cout << "Mouse up " << i << "\n";
         }
-
     }
 }
