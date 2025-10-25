1e20f6b59d7f7d6b782757a4219d5be74f50639f
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-10-26 00:57:51 +0200

    Added PrefabSystemTests + added wip prefab file

---
+++ Projects/UnitTests/Source/Tests/Prefabs/PrefabSystemTests.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Jeppe Nielsen on 25/10/2025.
+//
+
+#include "gtest/gtest.h"
+#include "HierarchySystem.hpp"
+#include "PrefabSystem.hpp"
+#include "ResourceManager.hpp"
+#include "PrefabResourceLoaderFactory.hpp"
+#include "RegistrySerializer.hpp"
+#include "LocalTransform.hpp"
+#include "MathReflection.hpp"
+
+using namespace LittleCore;
+
+namespace {
+
+    TEST(PrefabSystem, TestInstance) {
+
+        std::string rootPath = "../../../../Assets/";
+
+        RegistrySerializer<LocalTransform, Hierarchy> serializer;
+
+        ResourcePathMapper resourcePathMapper;
+        resourcePathMapper.RefreshFromRootPath(rootPath);
+        ResourceManager<PrefabResourceLoaderFactory> resourceManager(resourcePathMapper);
+        resourceManager.CreateLoaderFactory<PrefabResourceLoaderFactory>(serializer);
+
+        entt::registry registry;
+        HierarchySystem hierarchySystem(registry);
+        PrefabSystem prefabSystem(registry);
+
+        auto instanceEntity = registry.create();
+        auto& instanceHierarchy = registry.emplace<Hierarchy>(instanceEntity);
+        auto& prefabInstance = registry.emplace<PrefabInstance>(instanceEntity);
+        prefabInstance.Prefab = resourceManager.Create<PrefabResource>("9953944CCE324C019F30699342FF9AE0");
+
+        hierarchySystem.Update();
+        prefabSystem.Update();
+
+        EXPECT_TRUE(prefabInstance.root!=entt::null);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
