85157d3d1a2907f423cfc74800eea1652051ec4a
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-12-20 23:51:27 +0100

    added renderable reflection

--- Engine/LittleCore/Reflection/ComponentReflection.hpp
+++ Engine/LittleCore/Reflection/ComponentReflection.hpp
@@ -6,6 +6,7 @@
 #include <glaze/glaze.hpp>
 #include "Hierarchy.hpp"
 #include "Prefab.hpp"
+#include "Renderable.hpp"
 
 template<>
 struct glz::meta<LittleCore::Hierarchy> {
@@ -20,5 +21,14 @@ struct glz::meta<LittleCore::Prefab> {
     using T = LittleCore::Prefab;
     static constexpr auto value = glz::object(
             "resource", &T::resource
+    );
+};
+
+template<>
+struct glz::meta<LittleCore::Renderable> {
+    using T = LittleCore::Renderable;
+    static constexpr auto value = glz::object(
+            "shader", &T::shader,
+            "blendMode", &T::blendMode
     );
 };
