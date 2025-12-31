4af9a424037cfeea684bc147aeaf095280492268
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2025-12-31 01:45:54 +0100

    main files cleanup

--- Projects/TestNetimguiClient/Source/main.cpp
+++ Projects/TestNetimguiClient/Source/main.cpp
@@ -1,306 +1,89 @@
 //
-// Created by Jeppe Nielsen on 13/04/2024.
+// Created by Jeppe Nielsen on 27/12/2025.
 //
-/*
+
 #include "Engine.hpp"
-#include "EditorSimulations/EditorSimulationRegistry.hpp"
-#include "ImGuiController.hpp"
-#include <iostream>
-#include "NetimguiClientController.hpp"
-#include <thread>
-#include <SDL3/SDL.h>
+#include "MainState.hpp"
+#include <imgui.h>
 #include "DefaultSimulation.hpp"
-#include "SDLInputHandler.hpp"
-#include "BgfxRenderer.hpp"
-#include "DefaultResourceManager.hpp"
-#include <glm/glm.hpp>
-#include "MathReflection.hpp"
-#include "EntityGuiDrawer.hpp"
-#include "SystemBase.hpp"
-#include "InputRotationSystem.hpp"
-#include "MovableSystem.hpp"
-#include "PickingSystem.hpp"
-#include "Project.hpp"
-#include "ProjectWindow.hpp"
-#include "MeshLoader.hpp"
-#include "RegistrySerializer.hpp"
-#include "MetaHelper.hpp"
-#include "GuiResourceDrawers.hpp"
 #include "FileHelper.hpp"
-#include "TupleHelper.hpp"
-#include "PrefabSystem.hpp"
-#include "PrefabExposedComponents.hpp"
-#include "LabelMeshSystem.hpp"
 
 using namespace LittleCore;
 
-struct Rotatable {
-    float speed = 0.0f;
-    float speedY = 0.0f;
-    float speedX = 0;
-    float yaw =0;
+struct Velocity {
+    glm::vec3 speed;
 };
 
-struct RotationSystem : SystemBase {
+struct Rotater {
+    float speed;
+};
+
+struct MoverSystem : LittleCore::SystemBase {
 
     void Update(float dt) {
+        for(auto[entity, velocity, transform]  : registry.view<const Velocity, LocalTransform>().each()) {
+            transform.position += velocity.speed * dt;
+            registry.patch<LocalTransform>(entity);
+        }
+    }
+};
 
-        auto view = registry.view<Rotatable, LocalTransform>();
-        for(auto[entity, rotatable, transform] : view.each()) {
-            //auto roll = glm::roll(transform.rotation);
-            //auto yaw = glm::yaw(transform.rotation);
-            rotatable.yaw += dt * rotatable.speedY;
+struct RotatorSystem : LittleCore::SystemBase {
 
-            transform.rotation = glm::quat({0, rotatable.yaw,0});
+    void Update(float dt) {
+        for(auto[entity, rotator, transform]  : registry.view<const Rotater, LocalTransform>().each()) {
+            transform.rotation *= quat({0,rotator.speed * dt ,0 });
             registry.patch<LocalTransform>(entity);
         }
     }
 
 };
 
-struct Wobbler {
-    float speed = 0;
-    vec3 direction = vec3(0,1,0);
-    float position = 0;
-};
-
-struct WobblerSystem : SystemBase {
-
-    void Update(float dt) {
-        auto view = registry.view<LocalTransform, Wobbler>();
-        for(auto[entity, transform, wobbler] : view.each()) {
-            transform.position = vec3(0,0,0) + wobbler.direction * sin(wobbler.position);
-            wobbler.position += wobbler.speed * dt;
-            registry.patch<LocalTransform>(entity);
-        }
-    }
-};
-
-struct TestNetimguiClient : IState {
-    BGFXRenderer renderer;
-    EditorSimulationContext editorSimulationContext;
-    EditorSimulationRegistry editorSimulationRegistry;
-    NetimguiClientController netimguiClientController;
-    ImGuiController gui;
-    Project project;
-    ProjectWindow projectWindow;
-    DefaultResourceManager resourceManager;
-    EntityGuiDrawerContext drawerContext;
-
-    using Components = Meta::TypeList<
-            LocalTransform,
-            Wobbler,
-            Camera,
-            Rotatable,
-            Texturable,
-            Renderable,
-            Mesh,
-            Prefab,
-            PrefabExposedComponents,
-            Label,
-            Colorable
-            >;
-
-    Meta::Rebind<EntityGuiDrawer, Components> drawer;
-
-    using ComponentSerializers = Meta::TypeList<
-            WorldTransform,
-            LocalBoundingBox,
-            WorldBoundingBox,
-            Hierarchy>;
-
-    using SerializerComponents = Meta::Concat<Components, ComponentSerializers>;
-
-    Meta::Rebind<RegistrySerializer, SerializerComponents > registrySerializer;
 
 
-    CustomSimulation<RotationSystem, WobblerSystem, MovableSystem, InputRotationSystem> simulation;
+struct SimpleGame : public LittleCore::MainState {
 
+    LittleCore::CustomSimulation<MoverSystem> simulation;
 
-
-    TestNetimguiClient() :
-            drawerContext(resourceManager, registrySerializer),
-            drawer(drawerContext),
-    editorSimulationContext(renderer, netimguiClientController, drawer),
-    editorSimulationRegistry(editorSimulationContext),
-    resourceManager(project.resourcePathMapper) {}
-
-    entt::entity CreateQuadNew(entt::registry& registry, glm::vec3 position, glm::vec3 scale, entt::entity parent = entt::null) {
-
-        auto quad = registry.create();
-        auto& tr = registry.emplace<LocalTransform>(quad);
-        tr.position = position;
-        tr.scale = scale;
-        registry.emplace<WorldTransform>(quad);
-        registry.emplace<Hierarchy>(quad).parent = parent;
-        auto& mesh = registry.emplace<Mesh>(quad);
-
-        mesh.vertices.push_back({{-1,-1,0}, 0xFFFFFF , {0,0}});
-        mesh.vertices.push_back({{1,-1,0}, 0xFFFFFF , {0,1} });
-        mesh.vertices.push_back({{1,1,0}, 0xFFFFFF, {1,1}});
-        mesh.vertices.push_back({{-1,1,0}, 0x00FFFF,{1,0}});
-        mesh.triangles.push_back(0);
-        mesh.triangles.push_back(1);
-        mesh.triangles.push_back(2);
-
-        mesh.triangles.push_back(0);
-        mesh.triangles.push_back(2);
-        mesh.triangles.push_back(3);
-
-        registry.emplace<Renderable>(quad);
-        registry.emplace<LocalBoundingBox>(quad);
-        registry.emplace<WorldBoundingBox>(quad);
-        registry.emplace<Texturable>(quad);
-
-        registry.get<Renderable>(quad).shader = resourceManager.Create<ShaderResource>("65886F92DEC94836A9E2FEA6C3483543");
-        registry.get<Texturable>(quad).handle = resourceManager.Create<Texturable>("020CDC91085545C0AB724CC5C3645F3B");
-
-        return quad;
+    void OnInitialize() override {
+        SerializedTypes<Velocity, Rotater>();
+        AddSimulation(simulation);
     }
 
-    void Initialize() override {
-
-        editorSimulationRegistry.AddSimulation(simulation);
-
-        gui.Initialize(mainWindow, [this]() {
-            OnGUI();
-        });
-
-        gui.LoadFont("/Users/jeppe/Jeppes/LittleCore/Projects/TestImGui/Source/Fonts/LucidaG.ttf", 12);
-
-        netimguiClientController.Start();
-        netimguiClientController.Connect("Test client", "localhost");
-
-        while (netimguiClientController.IsConnectionPending()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds (16));
-        }
-        if (!netimguiClientController.IsConnected()) {
-            std::cout << "couldn't connect\n";
-        }
-
-        project.rootPath = "/Users/jeppe/Jeppes/LittleCore/Projects/TestNetimguiClient/Source/Assets/";
-        project.resourcePathMapper.RefreshFromRootPath(project.rootPath);
-
-        resourceManager.CreateLoaderFactory<ShaderResourceLoaderFactory>();
-        resourceManager.CreateLoaderFactory<TextureResourceLoaderFactory>();
-        resourceManager.CreateLoaderFactory<MeshResourceLoaderFactory>();
-        resourceManager.CreateLoaderFactory<PrefabResourceLoaderFactory>(registrySerializer, &resourceManager);
-        resourceManager.CreateLoaderFactory<FontResourceLoaderFactory>();
-
-        simulation.SetResources(registrySerializer, resourceManager);
-
-        auto& registry = simulation.registry;
-        {
-            auto cameraObject = registry.create();
-            registry.emplace<LocalTransform>(cameraObject).position = {0, 0, -10};
-            registry.emplace<WorldTransform>(cameraObject);
-            registry.emplace<Hierarchy>(cameraObject);
-
-            auto &camera = registry.emplace<Camera>(cameraObject);
-            camera.fieldOfView = 30.0f;
-            camera.near = 1;
-            camera.far = 200;
-            camera.viewRect = {{0,    0},
-                               {1.0f, 1.0f}};
-
-
-            auto& movable = registry.emplace<Movable>(cameraObject);
-            movable.speed = 10.0f;
-            movable.keys.push_back({
-                InputKey::A,
-                vec3(-1,0,0)
-            });
-            movable.keys.push_back({
-                                                    InputKey::D,
-                                                    vec3(1,0,0)
-                                            });
-            movable.keys.push_back({
-                                                    InputKey::W,
-                                                    vec3(0,0,1)
-                                            });
-            movable.keys.push_back({
-                                                    InputKey::S,
-                                                    vec3(0,0,-1)
-                                            });
-            registry.emplace<Input>(cameraObject);
-
-            auto& inputRotation = registry.emplace<InputRotation>(cameraObject);
-
-        }
-
-        auto quad = CreateQuadNew(registry, {0, 0, 0}, {1,1,1});
+    void OnUpdate(float dt) override {
+        simulation.Update(dt);
     }
 
-    void HandleEvent(void* event) override {
-        gui.HandleEvent(event);
+    void OnRender() override {
+
     }
 
-    void OnGUI() {
-        ImGui::DockSpaceOverViewport();
-
-        EditorSimulation* currentSimulation;
-        if (editorSimulationRegistry.TryGetFirst(&currentSimulation)) {
-            currentSimulation->DrawGUI();
-        }
-
-        projectWindow.Draw(project, resourceManager, editorSimulationRegistry);
-
-        ImGui::Begin("Save");
+    void OnGui() override {
+        ImGui::Begin("File");
 
         if (ImGui::Button("Save")) {
-
-            SerializationContext context {
-                .resourceManager = &resourceManager
-            };
-
-            auto data = registrySerializer.Serialize(simulation.registry, context);
-            std::cout << data << std::endl;
+            auto data = Save(simulation.registry);
             FileHelper::TryWriteAllText("Scene.json", data);
-
-            //registrySerializer.Deserialize()
-
         }
 
         if (ImGui::Button("Load")) {
-
             auto data = FileHelper::ReadAllText("Scene.json");
-
-            SerializationContext context {
-                    .resourceManager = &resourceManager
-            };
-
             simulation.registry.clear();
-            registrySerializer.Deserialize(simulation.registry, data, context);
-
-            //registrySerializer.Deserialize()
+            auto error = Load(simulation.registry, data);
 
         }
-
-
-
 
         ImGui::End();
 
     }
 
-    void Update(float dt) override {
-        EditorSimulation* currentSimulation;
-        if (editorSimulationRegistry.TryGetFirst(&currentSimulation)) {
-            currentSimulation->Update(dt);
-        }
-        simulation.Update(dt);
-    }
-
-    void Render() override {
-        gui.Render();
-    }
 };
 
-
-int main_editor() {
-    Engine e({"Netimgui Client", true});
-    e.Start<TestNetimguiClient>();
-    return 0;
+int main() {
+    using namespace LittleCore;
+    Engine engine({
+        .mainWindowTitle = "Editor",
+        .showWindow = true
+    });
+    engine.Start<SimpleGame>();
 }
-
- */
