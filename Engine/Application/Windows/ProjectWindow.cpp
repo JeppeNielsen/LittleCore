cccd596b454e885bbe3265b44da8f4a103b736fb
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2026-01-12 15:37:50 +0100

    project window now uses FileTree concept

--- Engine/Application/Windows/ProjectWindow.cpp
+++ Engine/Application/Windows/ProjectWindow.cpp
@@ -3,74 +3,17 @@
 //
 
 #include "ProjectWindow.hpp"
-
-
-
 #include <imgui.h>
-#include "ProjectWindow.hpp"
-#include <map>
 #include <string>
-#include <vector>
-#include <sstream>
-#include <algorithm>
 #include "PathHelper.hpp"
-#include <unordered_map>
+#include "FileTreeFactory.hpp"
 
 using namespace LittleCore;
 
 static std::string draggingResourcePath;
 
-struct Node {
-    std::string name;
-    std::string path;
-    std::string fullPath;
-    std::map<std::string, Node> children;
-    bool isFile = false; // Mark if this node represents a file
-};
-
-// Helper function to split a file path into components
-std::vector<std::string> splitPath(const std::string& path, char delimiter = '/') {
-    std::vector<std::string> components;
-    std::stringstream ss(path);
-    std::string part;
-    while (std::getline(ss, part, delimiter)) {
-        if (!part.empty()) {
-            components.push_back(part);
-        }
-    }
-    return components;
-}
-
-// Build the hierarchy tree from the file paths
-void buildTree(Node& root, const std::unordered_map<std::string, std::string>& filePaths, const std::size_t cutoffPath) {
-    for (const auto& pathKeyValue : filePaths) {
-        std::string fullPath = pathKeyValue.first;
-        std::string path = pathKeyValue.first.substr(cutoffPath);
-        auto components = splitPath(path);
-        Node* current = &root;
-        for (size_t i = 0; i < components.size(); ++i) {
-            const auto& part = components[i];
-            if (i == components.size() - 1) {
-                // Mark the last component as a file
-
-                auto& file = current->children[part];
-                file.name = part;
-                file.path = path;
-                file.fullPath = fullPath;
-            } else {
-                // Navigate or create the directory node
-                current = &current->children[part];
-                current->name = part;
-                current->path = path;
-                current->fullPath = fullPath;
-            }
-        }
-    }
-}
-
 // Recursive function to traverse and print the hierarchy
-void traverseTree(const Node& node, const std::string& prefix = "") {
-
+void traverseTree(const FileTreeNode& node, const std::string& prefix = "") {
 
     ImGuiTreeNodeFlags nodeFlags = ImGuiTreeNodeFlags_None;
 
@@ -128,9 +71,10 @@ void ProjectWindow::Draw(Project& project, DefaultResourceManager& resourceManag
     }
 
 
-    Node root;
+    FileTreeNode root;
+    FileTreeFactory fileTreeFactory;
     root.name = "Project";
-    buildTree(root, project.resourcePathMapper.PathToGuids(), project.rootPath.size());
+    fileTreeFactory.CreateTree(root, project.resourcePathMapper.PathToGuids(), project.rootPath);
     traverseTree(root);
 
     ImGui::End();
