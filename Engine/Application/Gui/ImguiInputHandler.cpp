37c51ee0e1e2d5a332861258045979270c32f7fb
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2026-05-16 01:29:44 +0200

    fixed imgui keys out of bounds

--- Engine/Application/Gui/ImguiInputHandler.cpp
+++ Engine/Application/Gui/ImguiInputHandler.cpp
@@ -28,7 +28,7 @@ void ImguiInputHandler::HandleInput(void* event, Input& input) {
 
     ImGuiIO& io = ImGui::GetIO();
 
-    for (int i = 0; i < ImGuiKey_COUNT; ++i) {
+    for (int i = ImGuiKey_NamedKey_BEGIN; i < ImGuiKey_NamedKey_END; ++i) {
         if (ImGui::IsKeyPressed((ImGuiKey)i, false)) {
             input.keysDown.push_back((InputKey)i);
         }
